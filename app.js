7eb06b5661c2f664914bf345d591663880710a95
unknown <pete.carey@yahoo.co.uk>
2020-12-22 22:54:39 +0000

    hange of vrm

--- app.js
+++ app.js
@@ -21,6 +21,21 @@ var jaguar = {
         // carAge = chosenDate - RegistrationDate;
     },
 };
+var a, b, c;
+var olderMth;
+var olderYr;
+var moreRecentMth;
+var moreRecentYr;
+var durMonths;
+var durYears;
+var totalDurYears;
+var remainderDurMths;
+var totalDurMonths;
+var mileage;
+var averageMileage;
+var noMileageProvidedCalc;
+var mot_mileage;
+var approx_mileage;
 console.log(jaguar.vrm);
 console.log(jaguar.model);
 console.log(jaguar.getAnnualMileage());
@@ -29,19 +44,18 @@ function subtract(olderDate, moreRecentDate, whereTo) {
     console.log(moreRecentDate);
     console.log("whereTo is " + whereTo);
     debugger;
-    var a, b, c;
-    var olderMth = 0;
-    var olderYr = 0;
-    var moreRecentMth = 0;
-    var moreRecentYr = 0;
-    var durMonths = 0;
-    var durYears = 0;
-    var totalDurMonths = 0;
-    var totalDurYears = 0;
-    var remainderDurMths = 0;
     a = 0;
     b = 0;
     c = 0;
+    olderMth = 0;
+    olderYr = 0;
+    moreRecentMth = 0;
+    moreRecentYr = 0;
+    durMonths = 0;
+    durYears = 0;
+    totalDurYears = 0;
+    remainderDurMths = 0;
+    totalDurMonths = 0;
     a = String(document.getElementById(olderDate).value);
     olderMth = a.substr(0, 2);
     console.info("olderMth is " + olderMth);
@@ -62,6 +76,7 @@ function subtract(olderDate, moreRecentDate, whereTo) {
     console.info("totalDurMonths is " + totalDurMonths);
     totalDurYears = Math.trunc(totalDurMonths / 12);
     console.info("totalDurYears " + totalDurYears);
+    // find out remainder
     remainderDurMths = totalDurMonths % 12;
     /*
     if (remainderDurMths > 0) {
@@ -70,7 +85,45 @@ function subtract(olderDate, moreRecentDate, whereTo) {
     */
     console.info("remainderDurMths " + remainderDurMths);
     document.getElementById(whereTo).value =
-        totalDurYears + " years and " + remainderDurMths + " months";
+        totalDurYears + " years " + remainderDurMths + " months";
+}
+function calculateMileage(mileage, whereTo) {
+    debugger;
+    console.log("mileage is " + mileage);
+    console.log("totalDurMonths is " + totalDurMonths);
+    averageMileage = Math.trunc(mileage / totalDurMonths) * 12;
+    console.log("averageMileage is " + averageMileage);
+    document.getElementById(whereTo).value = String(averageMileage);
+}
+function calculateDurationMonths(moreRecentDate, olderDate) {
+    noMileageProvidedCalc = 0;
+    mot_mileage = 0;
+    approx_mileage = 0;
+    a = String(document.getElementById(olderDate).value);
+    olderMth = a.substr(0, 2);
+    console.info("olderMth is " + olderMth);
+    olderYr = a.slice(2, 6);
+    console.info("olderYr is " + olderYr);
+    b = String(document.getElementById(moreRecentDate).value);
+    moreRecentMth = b.substr(0, 2);
+    console.log("(moreRecentMth is " + moreRecentMth);
+    moreRecentYr = b.slice(2, 6);
+    console.log("moreRecentYr is " + moreRecentYr);
+    durMonths = moreRecentMth - olderMth;
+    console.log("durMonths is " + durMonths);
+    durYears = moreRecentYr - olderYr;
+    console.log("durYears is " + durYears);
+    // convert to months
+    totalDurMonths = durYears * 12;
+    totalDurMonths = totalDurMonths + durMonths;
+    console.info("totalDurMonths is " + totalDurMonths);
+    totalDurYears = Math.trunc(totalDurMonths / 12);
+    console.info("totalDurYears " + totalDurYears);
+    noMileageProvidedCalc = Math.trunc((totalDurMonths / 12) * 7900);
+    console.log("noMileageProvidedCalc is " + noMileageProvidedCalc);
+    mot_mileage = Number(document.getElementById("mot_mileage").value);
+    console.log("mot_mileage is " + mot_mileage);
+    approx_mileage = noMileageProvidedCalc + mot_mileage;
 }
 function myFunction(id) {
     console.log("test" + id);
--- app.ts
+++ app.ts
@@ -45,6 +45,23 @@ var jaguar: Car = {
     // carAge = chosenDate - RegistrationDate;
   },
 };
+let a, b, c;
+let olderMth: number;
+let olderYr: number;
+let moreRecentMth: number;
+let moreRecentYr: number;
+let durMonths: number;
+let durYears: number;
+
+let totalDurYears;
+let remainderDurMths;
+
+let totalDurMonths: number;
+let mileage: number;
+let averageMileage: number;
+let noMileageProvidedCalc: number;
+let mot_mileage: number;
+let approx_mileage: number;
 
 console.log(jaguar.vrm);
 console.log(jaguar.model);
@@ -55,19 +72,20 @@ function subtract(olderDate, moreRecentDate, whereTo) {
   console.log(moreRecentDate);
   console.log("whereTo is " + whereTo);
   debugger;
-  let a, b, c;
-  let olderMth: number = 0;
-  let olderYr: number = 0;
-  let moreRecentMth: number = 0;
-  let moreRecentYr: number = 0;
-  let durMonths: number = 0;
-  let durYears: number = 0;
-  let totalDurMonths: number = 0;
-  let totalDurYears: number = 0;
-  let remainderDurMths: number = 0;
+
   a = 0;
   b = 0;
   c = 0;
+  olderMth = 0;
+  olderYr = 0;
+  moreRecentMth = 0;
+  moreRecentYr = 0;
+  durMonths = 0;
+  durYears = 0;
+
+  totalDurYears = 0;
+  remainderDurMths = 0;
+  totalDurMonths = 0;
 
   a = String((<HTMLInputElement>document.getElementById(olderDate)).value);
   olderMth = a.substr(0, 2);
@@ -90,6 +108,7 @@ function subtract(olderDate, moreRecentDate, whereTo) {
   console.info("totalDurMonths is " + totalDurMonths);
   totalDurYears = Math.trunc(totalDurMonths / 12);
   console.info("totalDurYears " + totalDurYears);
+  // find out remainder
   remainderDurMths = totalDurMonths % 12;
   /*
   if (remainderDurMths > 0) {
@@ -98,9 +117,54 @@ function subtract(olderDate, moreRecentDate, whereTo) {
   */
   console.info("remainderDurMths " + remainderDurMths);
   (<HTMLInputElement>document.getElementById(whereTo)).value =
-    totalDurYears + " years and " + remainderDurMths + " months";
+    totalDurYears + " years " + remainderDurMths + " months";
 }
 
+function calculateMileage(mileage, whereTo) {
+  debugger;
+  console.log("mileage is " + mileage);
+  console.log("totalDurMonths is " + totalDurMonths);
+
+  averageMileage = Math.trunc(mileage / totalDurMonths) * 12;
+  console.log("averageMileage is " + averageMileage);
+  (<HTMLInputElement>document.getElementById(whereTo)).value = String(
+    averageMileage
+  );
+}
+
+function calculateDurationMonths(moreRecentDate, olderDate) {
+  noMileageProvidedCalc = 0;
+  mot_mileage = 0;
+  approx_mileage = 0;
+  a = String((<HTMLInputElement>document.getElementById(olderDate)).value);
+  olderMth = a.substr(0, 2);
+  console.info("olderMth is " + olderMth);
+  olderYr = a.slice(2, 6);
+  console.info("olderYr is " + olderYr);
+
+  b = String((<HTMLInputElement>document.getElementById(moreRecentDate)).value);
+  moreRecentMth = b.substr(0, 2);
+  console.log("(moreRecentMth is " + moreRecentMth);
+  moreRecentYr = b.slice(2, 6);
+  console.log("moreRecentYr is " + moreRecentYr);
+  durMonths = moreRecentMth - olderMth;
+  console.log("durMonths is " + durMonths);
+  durYears = moreRecentYr - olderYr;
+  console.log("durYears is " + durYears);
+  // convert to months
+  totalDurMonths = durYears * 12;
+  totalDurMonths = totalDurMonths + durMonths;
+  console.info("totalDurMonths is " + totalDurMonths);
+  totalDurYears = Math.trunc(totalDurMonths / 12);
+  console.info("totalDurYears " + totalDurYears);
+  noMileageProvidedCalc = Math.trunc((totalDurMonths / 12) * 7900);
+  console.log("noMileageProvidedCalc is " + noMileageProvidedCalc);
+  mot_mileage = Number(
+    (<HTMLInputElement>document.getElementById("mot_mileage")).value
+  );
+  console.log("mot_mileage is " + mot_mileage);
+  approx_mileage = noMileageProvidedCalc + mot_mileage;
+}
 function myFunction(id) {
   console.log("test" + id);
 }
